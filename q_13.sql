e1589e6b7b533a01833981c6fbaa7597552607a5
Rohan Kumar <rk8810427573@gmail.com>
2026-03-05 01:22:09 +0530

    Add files via upload

--- q_13.sql
+++ q_13.sql
@@ -1,6 +1,8 @@
 -- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
 select category , name,revenue,rn
 from
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
+
 (select name , category,revenue, rank() over (partition by  category order by revenue) as rn
 from
 (select z.name , z.category , round(sum(p.price * d.quantity)) as revenue
